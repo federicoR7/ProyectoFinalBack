3528d6bfcfdd9831a30e96cc21f2e3fd3d862c8a
Federico Rabanedo <federico.rabanedo@gmail.com>
2024-11-24 16:17:53 -0300

    cambios 1.2

--- front/src/componentes/api/Register.jsx
+++ front/src/componentes/api/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import api from './api';
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
   const [username, setUsername] = useState('');
