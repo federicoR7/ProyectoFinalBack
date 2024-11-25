8b4251141dbd92838e03ea3b1c39da70f4461a1b
Federico Rabanedo <federico.rabanedo@gmail.com>
2024-11-25 15:52:52 -0300

    new changes

--- back/server.js
+++ back/server.js
@@ -12,6 +12,13 @@ app.use(cors());
 app.use(express.json()); 
 app.use(express.static(path.join(__dirname, 'build')));
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
 
 // Configuración de la sesión 
 app.use(session({
