be3fc7f563147dfd9561430629be79c3cfe26595
federicoR7 <federico.rabanedo@gmail.com>
2024-11-27 12:55:58 -0300

    saque el index de patch

--- back/server.js
+++ back/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build'));
 });
 
 app.use((req, res, next) => {
