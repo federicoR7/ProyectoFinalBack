596721112262534039075f04fe25eb6de4202a4a
federicoR7 <federico.rabanedo@gmail.com>
2024-11-27 13:51:20 -0300

    cambio

--- back/server.js
+++ back/server.js
@@ -17,9 +17,9 @@ app.use(express.json());
 const path = require('path');
 
 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, '../front/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../front/build', 'index.html'));
 });
 // app.use(express.static(path.join(__dirname, 'build')));
 // app.get('*', (req, res) => {
