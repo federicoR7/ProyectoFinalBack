edbfa25ac044faf712500a7bbe201abfaf0124a7
federicoR7 <federico.rabanedo@gmail.com>
2024-11-27 13:22:09 -0300

    paso atras

--- back/server.js
+++ back/server.js
@@ -11,16 +11,10 @@ const app = express();
 app.use(cors()); 
 app.use(express.json()); 
 
- const path = require('path');
-// app.use(express.static(path.join(__dirname, 'build')));
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-
-
-app.use(express.static(path.join(__dirname, '..', 'frontend', 'build'))); // Asegúrate de que 'frontend' sea el nombre correcto de tu carpeta
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html')); // Sirve el archivo 'index.html' de React
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.use((req, res, next) => {
